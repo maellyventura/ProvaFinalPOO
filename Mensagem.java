2c74e4618df1538e0083729802f6680f13ed8040
maellyventura <barbosamaelly@gmail.com>
2018-03-09 10:17:08 -0300

    Create Mensagem.java

---
+++ Mensagem.java
@@ -0,0 +1,29 @@
+public abstract class Mensagem<WTTexxtual> {
+	private String remetente;
+	private WTTexxtual destinatario;
+	protected String conteudo;
+	
+	@SuppressWarnings("rawtypes")
+	public Mensagem (String remetente, WTTextual destinario){
+		
+	}
+	public String getRemetente(){
+		return remetente;
+		
+	}
+	public WTTexxtual getDestinatatio(){
+		return destinatario;
+		
+	}
+	public String verConteudo(){
+		return conteudo;
+		
+	}
+   public void LongTextMsg (String remetente, WTTextual destinatario, String texto, Object Mensagem){
+	   Mensagem = null;
+	   
+		   
+	   }
+   }
+
+
